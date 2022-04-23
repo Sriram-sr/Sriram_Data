665044915031c43a669bd34290a618930d35e8b5
Sriram <srirampanneer90@gmail.com>
2022-04-23 12:18:43 +0530

    'hg'

--- Selenium_Data/Concepts/checkbox_test.py
+++ Selenium_Data/Concepts/checkbox_test.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-driver = webdriver.Chrome(executable_path = '/home/sriram/Selenium/chromedriver')
+driver = webdriver.Chrome()
+driver.maximize_window()
+
 
 driver.get('http://automationpractice.com/index.php')
 driver.find_element_by_xpath('//*[@id="block_top_menu"]/ul/li[1]/a').click() #Finds a element by using xpath
 
 tickbox = driver.find_element_by_css_selector("input[name=layered_category_8]") #Finds a checkbox using \
-        #css_selector bcoz all checkboxes having same type='checkbox'
+        # css_selector bcoz all checkboxes having same type='checkbox'
 
 driver.find_element_by_css_selector("input[name=layered_category_8]").click()
 
-print(tickbox.is_selected()
+print(tickbox.is_selected())
 
